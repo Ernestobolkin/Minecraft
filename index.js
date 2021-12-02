beafd97e3aa372009c5231da28d170bd1df59ac2
Ernestobolkin <ernest1047@gmail.com>
2021-12-02 14:50:12 +0200

    adding some functions

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const table = document.querySelector("#newWorld");
 
+const materials = {
+  wood: "oak",
+  clouds: "cloud",
+  dirt: "dirt",
+  grass: "grass",
+  cloud: "cloud",
+  leaves: "leaves",
+  stone: "Stone",
+};
+const tools = {
+  axe: 24,
+  picaxe: 87,
+  shovel: 32,
+}
+
 const inventory = [];
 for (let i = 0; i < 20; i++) {
   const tr = document.createElement("tr");
@@ -8,42 +23,74 @@ for (let i = 0; i < 20; i++) {
   for (let j = 0; j < 20; j++) {
     const td = document.createElement("td");
     const div = document.createElement("div");
-    div.id = "x:" + j + "_" + "y:" + i ;
+    div.id = "x:" + j + "_" + "y:" + i;
     div.className = "block";
     td.appendChild(div);
-    div.sy
+    div.sy;
     div.addEventListener("click", () => {
-      if(div.classList.value !== "block"){
-      inventory.push(div.classList.value)
-      console.log(div.classList.value);
-      console.log(inventory);
-      div.classList= ""
+      if (div.classList.value !== "block") {
+        inventory.push(div.classList.value);
+        console.log(div.classList.value);
+        console.log(inventory);
+        div.classList = "";
       }
     });
     tr.appendChild(td);
     if (i === 15) {
       div.className = "grass";
-      div.style.cursor = "pointer"
     }
     if (i >= 16) {
       div.className = "dirt";
-      div.style.cursor = "pointer"
     }
   }
 }
 
-// const matirials = {
-//   let cloud = document.className = "cloud"
-// }
-
-// console.log(document.getElementById("x:8_y:3"));
-document.getElementById("x:3_y:5").id = "cloud";
-document.getElementById("x:4_y:5").id = "cloud";
-document.getElementById("x:5_y:5").id = "cloud";
-document.getElementById("x:2_y:6").id = "cloud";
-document.getElementById("x:3_y:6").id = "cloud";
-document.getElementById("x:4_y:6").id = "cloud";
-document.getElementById("x:5_y:6").id = "cloud";
-document.getElementById("x:6_y:6").id = "cloud";
-
-
+const create = (x, y, type) => {
+  if (type === "stone") {
+    document.getElementById(`x:${x}_y:${y}`).classList.add(materials.stone);
+  } else if (type === "wood") {
+    document.getElementById(`x:${x}_y:${y}`).classList.add(materials.wood);
+  } else if (type === "leaves") {
+    document.getElementById(`x:${x}_y:${y}`).classList.add(materials.leaves);
+  } else if (type === "cloud") {
+    document.getElementById(`x:${x}_y:${y}`).id = materials.cloud;
+  }
+};
+// stone
+create(13, 13, "stone");
+create(13, 14, "stone");
+create(14, 13, "stone");
+create(14, 14, "stone");
+// wood
+create(4, 11, "wood");
+create(4, 12, "wood");
+create(4, 13, "wood");
+create(4, 14, "wood");
+// leaves
+create(2, 10, "leaves");
+create(3, 10, "leaves");
+create(4, 10, "leaves");
+create(5, 10, "leaves");
+create(6, 10, "leaves");
+create(2, 9, "leaves");
+create(3, 9, "leaves");
+create(4, 9, "leaves");
+create(5, 9, "leaves");
+create(6, 9, "leaves");
+create(2, 8, "leaves");
+create(3, 8, "leaves");
+create(4, 8, "leaves");
+create(5, 8, "leaves");
+create(6, 8, "leaves");
+create(3, 7, "leaves");
+create(4, 7, "leaves");
+create(5, 7, "leaves");
+// clouds
+create(3,2,"cloud")
+create(4,2,"cloud")
+create(5,2,"cloud")
+create(2,3,"cloud")
+create(3,3,"cloud")
+create(4,3,"cloud")
+create(5,3,"cloud")
+create(6,3,"cloud")
